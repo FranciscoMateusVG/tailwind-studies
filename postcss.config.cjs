1021d4306f96a4d13c1b60b3cb7f4c1efd65dc6b
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2023-12-21 12:34:09 -0300

    random changes

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const postcssPresetEnv = require("postcss-preset-env");
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-preset-env": {},
   },
 };
